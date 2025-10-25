4fe4711970be285fe4a8415d19e67def47248783
JosebaIncera <josebainc@hotmail.com>
2025-10-25 02:29:33 +0200

    config fixes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
-    // Disable static generation for pages that need runtime environment variables
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-    },
 }
 
 module.exports = nextConfig
